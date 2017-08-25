a095347ab6299041fbbe5efabec326789483ec8e
Kevin Hou <kevin.ch.hou@gmail.com>
2017-08-24 23:48:38 -0700

    Updated VSCO latest images

--- vscoImageGrabber.py
+++ vscoImageGrabber.py
@@ -17,7 +17,11 @@
 
 ################   User Settings   ################
 # Ignore the profile picture
-profilePicture = "im.vsco.co/1/5596ee97bbcb95169892/575bb69d8c303b81438b456d/vsco_061016.jpg?w=100&amp;h=100&amp;fit=crop&amp;dpr=2"
+outputFileName = "vscoImages.js"
+profilePictures = [
+    "im.vsco.co/1/5596ee97bbcb95169892/575bb69d8c303b81438b456d/vsco_061016.jpg?w=100&amp;h=100&amp;fit=crop&amp;dpr=1",
+    "im.vsco.co/1/5596ee97bbcb95169892/575bb69d8c303b81438b456d/vsco_061016.jpg?w=100&amp;h=100&amp;fit=crop&amp;dpr=2"
+]
 desiredWidth = "1200" # VSCO serves uses 300px width on the user's page
 
 ################   Imports   ################
@@ -28,6 +32,8 @@ import webbrowser # This module can control the browser
 import fileinput # To read files from input
 import glob # Grabbing files with extensions
 import os # Searching through computer directory
+import io
+from datetime import datetime # For timestamps
 
 ################   Find all VSCO pages   ################
 files = [] # Valid files
@@ -42,7 +48,6 @@ images = [] # URLs to all images
 tagID = "<img alt=\"\" src=\"//"
 endTagID = "\" width=\""
 
-print "const vscoImages = ["; # Open array
 # Cycle through each file
 for fileName in files:
     file = open(fileName, "r") # Open the file
@@ -53,7 +58,26 @@ for fileName in files:
             endIndex = line.index(endTagID) # Get end position of link
             imgTag = line[startIndex:endIndex] # Cut the front of the line
 
-            if imgTag != profilePicture: # If not the profile picture
-                largeImg = imgTag.replace("300", desiredWidth) # Change to 1200 resolution
-                print "  \"" + largeImg + "\"," # Print
-print "];"; # Open array
+            if imgTag not in profilePictures: # If not the profile picture
+                if imgTag not in images: # Make sure don't add duplicates
+                    largeImg = imgTag.replace("300", desiredWidth) # Change to 1200 resolution
+                    images.append(largeImg) # Add to master array
+    
+    file.close()
+
+################   Output to JS File   ################
+print "{0} images found".format(images.count)
+try:
+    with io.FileIO(outputFileName, 'w') as outputFile: # Writing file and creating file if it doesn't exist
+        print "// Last updated: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print "const vscoImages = ["
+        outputFile.write("// Last updated: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')) # Timestamp
+        outputFile.write("const vscoImages = [") # Open array
+        for image in images:
+            outputFile.write("  \"" + image + "\",") # Print
+            print "  \"" + image + "\","
+        outputFile.write("];") # Open array
+        print "];"
+        outputFile.close()
+except IOError as (errno, strerror):
+    print "I/O error({0}): {1}".format(errno, strerror)
